15e45fd88e46bf3c88825608d0c498e1f70a994a
Haoze Wu <jose@Haozes-MacBook-Air.local>
2023-12-16 18:09:41 -0500

    Update Cloud Redis

--- utils/redisFactory.js
+++ utils/redisFactory.js
@@ -8,10 +8,22 @@ const redisFactory = (() => {
     const host = process.env.REDIS_HOST || 'localhost';
     const port = process.env.REDIS_PORT || 6379;
     process.stdout.write(`Connecting to Redis on ${host}:${port}\n`);
-    const client = redis.createClient({
-      host,
-      port,
-    });
+    let client;
+    if (host !== 'localhost') {
+      client = redis.createClient({
+        password: process.env.REDIS_PASSWORD,
+        socket: {
+          host,
+          port,
+        },
+      });
+    } else {
+      client = redis.createClient({
+        host,
+        port,
+      });
+    }
+
     await client.connect();
     client.on('connect', () => {
       process.stdout.write(`Redis client connected on ${host}:${port}\n`);
